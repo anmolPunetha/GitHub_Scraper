8188a7a3ec8c4a0bb5a36b770494e2f9c973a7fa
Anmol Punetha <60095765+anmolPunetha@users.noreply.github.com>
2021-09-10 09:24:14 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,3 @@
+gsheet_scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+                "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+webdriver_path = "C:/Program Files (x86)/chromedriver.exe"
